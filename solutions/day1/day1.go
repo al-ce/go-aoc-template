d6773e4ab298f8bfac0b643fdb42df56185de3a1
al-ce <aceserani@gmail.com>
2024-12-05 00:43:17 -0600

    chore(example): include sample import, return TODO instead of empty string

--- solutions/day1/day1.go
+++ solutions/day1/day1.go
@@ -1,9 +1,13 @@
 package day1
 
+// import (
+// 	h "go-aoc2024/internal/helpers"
+// )
+
 func Part1() string {
-	return ""
+	return "TODO"
 }
 
 func Part2() string {
-	return ""
+	return "TODO"
 }
