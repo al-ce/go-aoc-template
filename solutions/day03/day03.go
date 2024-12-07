d5924eedd921bad43e106ccb5078452c674e0ebc
al-ce <aceserani@gmail.com>
2024-12-06 22:43:58 -0600

    feat(solutions): solve 3-1

--- solutions/day03/day03.go
+++ solutions/day03/day03.go
@@ -1,13 +1,26 @@
 package day03
 
-// import (
-//     h "go-aoc-template/internal/helpers"
-// )
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+)
 
 func PartOne(lines []string) string {
-    return "TODO"
+	program := strings.Join(lines[:], "")
+	pattern := regexp.MustCompile(`mul\((\d{1,3}),(\d{1,3})\)`)
+	matches := pattern.FindAllStringSubmatch(program, -1)
+
+	sum := 0
+	for _, match := range matches {
+		left, _ := strconv.Atoi(match[1])
+		right, _ := strconv.Atoi(match[2])
+		sum += left * right
+	}
+	return fmt.Sprintf("%d", sum)
 }
 
 func PartTwo(lines []string) string {
-    return "TODO"
+	return "TODO"
 }
