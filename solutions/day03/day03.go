a98bb259cea9a8ec2d5b945df6374f38ef4b6b1e
al-ce <aceserani@gmail.com>
2024-12-06 23:31:13 -0600

    feat(solutions): solve 3-2

--- solutions/day03/day03.go
+++ solutions/day03/day03.go
@@ -7,20 +7,28 @@ import (
 	"strings"
 )
 
-func PartOne(lines []string) string {
-	program := strings.Join(lines[:], "")
-	pattern := regexp.MustCompile(`mul\((\d{1,3}),(\d{1,3})\)`)
+func runProgram(program string, pattern *regexp.Regexp) int {
 	matches := pattern.FindAllStringSubmatch(program, -1)
-
 	sum := 0
 	for _, match := range matches {
 		left, _ := strconv.Atoi(match[1])
 		right, _ := strconv.Atoi(match[2])
 		sum += left * right
 	}
+	return sum
+}
+
+func PartOne(lines []string) string {
+	program := strings.Join(lines[:], "")
+	pattern := regexp.MustCompile(`mul\((\d{1,3}),(\d{1,3})\)`)
+	sum := runProgram(program, pattern)
 	return fmt.Sprintf("%d", sum)
 }
 
 func PartTwo(lines []string) string {
-	return "TODO"
+	program := strings.Join(lines[:], "")
+	// Ignore any instructions between don't() and do()
+	pattern := regexp.MustCompile(`don't\(\).*?do\(\)`)
+	program = pattern.ReplaceAllString(program, "")
+	return PartOne(append([]string{}, program))
 }
--- solutions/day03/day03_test.go
+++ solutions/day03/day03_test.go
@@ -5,20 +5,22 @@ import (
     "testing"
 )
 
-var lines = strings.Split(`xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`, "\n")
+var example1 = strings.Split(`xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`, "\n")
+
+var example2 = strings.Split("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))", "\n")
 
 var partOneAnswer = "161"
-var partTwoAnswer = "example answer"
+var partTwoAnswer = "48"
 
-func TestPartOne(t *testing.T) {
-    solution := PartOne(lines)
-    if solution != partOneAnswer {
-        t.Errorf("Day 3 Part 1: \nexpected %v got %v", partOneAnswer, solution)
-    }
-}
+// func TestPartOne(t *testing.T) {
+//     solution := PartOne(example1)
+//     if solution != partOneAnswer {
+//         t.Errorf("Day 3 Part 1: \nexpected %v got %v", partOneAnswer, solution)
+//     }
+// }
 
 func TestPartTwo(t *testing.T) {
-    solution := PartTwo(lines)
+    solution := PartTwo(example2)
     if solution != partTwoAnswer {
         t.Errorf("Day 3 Part 2: \nexpected %v got %v", partTwoAnswer, solution)
     }
