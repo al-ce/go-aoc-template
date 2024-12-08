82c474208e9282f97b02487fd34ed5837da3d35e
al-ce <aceserani@gmail.com>
2024-12-07 21:24:47 -0600

    refactor!(main): require a single arg (<day>); print both solutions

--- main.go
+++ main.go
@@ -5,14 +5,15 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"time"
 
 	h "go-aoc-template/internal/helpers"
 	solutions "go-aoc-template/internal/imports"
 )
 
-func parser(args []string) (int, int) {
-	if len(args) != 2 {
-		fmt.Fprintf(os.Stderr, "Need exactly two args: <day> <part>\n")
+func parser(args []string) int {
+	if len(args) != 1 {
+		fmt.Fprintf(os.Stderr, "usage: %s <day>\n", os.Args[0])
 		os.Exit(1)
 	}
 
@@ -26,32 +27,29 @@ func parser(args []string) (int, int) {
 		os.Exit(1)
 	}
 
-	part, err := strconv.Atoi(args[1])
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "could not parse %s\n", args[0])
-		os.Exit(1)
-	}
-	if !(part == 1 || part == 2) {
-		fmt.Fprintf(os.Stderr, "%s: part must be 1 or 2\n", args[0])
-		os.Exit(1)
-	}
+	return day
+}
 
-	return day, part
+func runSolution(day int, part int, lines []string) {
+	input := solutions.InputCoords{Day: day, Part: part}
+	solutionFunc, exists := solutions.Solutions[input]
+	if !exists {
+		fmt.Fprintf(os.Stderr, "No solution implemented for day %d\n", day)
+		os.Exit(1)
+	}
+	timelog_start_1 := time.Now()
+	solution := solutionFunc(lines)
+	elapsed := time.Since(timelog_start_1)
+	fmt.Printf("Part %d: %s (%v)\n", part, solution, elapsed)
 }
 
 func main() {
 	flag.Parse()
 	args := flag.Args()
-	day, part := parser(args)
-	input := solutions.InputCoords{Day: day, Part: part}
-
-	solution, exists := solutions.Solutions[input]
-	if !exists {
-		fmt.Fprintf(os.Stderr, "No solution implemented for day %d\n", day)
-		os.Exit(1)
-	}
-
+	day := parser(args)
 	lines, err := h.InputLines(day)
 	h.Check(err)
-	fmt.Println(solution(lines))
+
+	runSolution(day, 1, lines)
+	runSolution(day, 2, lines)
 }
