a1c570a646d92a38adfd1c80ad5db278e9aa2712
al-ce <aceserani@gmail.com>
2024-12-08 00:20:52 -0600

    feat(solutions): solve 4-2

--- solutions/day04/day04.go
+++ solutions/day04/day04.go
@@ -1,38 +1,56 @@
 package day04
 
-import "fmt"
+import (
+	"fmt"
 
-const xmas = "XMAS"
+	h "go-aoc-template/internal/helpers"
+)
+
+const (
+	xmas = "XMAS"
+	MAS  = "MAS"
+)
 
 type Direction struct {
-	x int
 	y int
+	x int
 }
 
-var directions = []Direction{
-	{0, -1},  // N
-	{0, +1},  // S
-	{+1, 0},  // E
-	{-1, 0},  // W
+type Coords struct {
+	row int
+	col int
+}
+
+var ordinals = []Direction{
 	{+1, +1}, // NE
 	{+1, -1}, // SE
 	{-1, +1}, // NW
 	{-1, -1}, // SW
 }
 
-func isDirectionValid(lines []string, rowIdx, colIdx int, dir Direction) bool {
-	min, max := len(xmas)-1, len(lines)-len(xmas)
-	return !((dir.y == -1 && rowIdx < min) ||
-		(dir.y == +1 && rowIdx > max) ||
-		(dir.x == -1 && colIdx < min) ||
-		(dir.x == +1 && colIdx > max))
+var cardinals = []Direction{
+	{0, -1}, // N
+	{0, +1}, // S
+	{+1, 0}, // E
+	{-1, 0}, // W
 }
 
-func findXmas(lines []string, rowIdx, colIdx int, dir Direction) int {
-	for i, char := range xmas {
-		newRow := rowIdx + dir.y*i
-		newCol := colIdx + dir.x*i
-		curr := rune(lines[newRow][newCol])
+func isDirectionInBounds(lines []string, word string, c Coords, dir Direction) bool {
+	min, max := len(word)-1, len(lines)-len(word)
+	return !(!h.InMatrix(c.row, c.col, lines) ||
+		(dir.y == -1 && c.row < min) ||
+		(dir.y == +1 && c.row > max) ||
+		(dir.x == -1 && c.col < min) ||
+		(dir.x == +1 && c.col > max))
+}
+
+func findWord(lines []string, word string, c Coords, dir Direction) int {
+	if !isDirectionInBounds(lines, word, c, dir) {
+		return 0
+	}
+	for i, char := range word {
+		next := Coords{c.row + dir.y*i, c.col + dir.x*i}
+		curr := rune(lines[next.row][next.col])
 		if curr != char {
 			return 0
 		}
@@ -40,29 +58,39 @@ func findXmas(lines []string, rowIdx, colIdx int, dir Direction) int {
 	return 1
 }
 
-func checkCoordinate(lines []string, rowIdx, colIdx int) int {
-	found := 0
-	for _, dir := range directions {
-		valid := isDirectionValid(lines, rowIdx, colIdx, dir)
-		if !valid {
-			continue
-		}
-		found += findXmas(lines, rowIdx, colIdx, dir)
-	}
-	return found
-}
-
 func PartOne(lines []string) string {
+	directions := append(ordinals, cardinals...)
 	sum := 0
 	for rowIdx, row := range lines {
 		for colIdx := range len(row) {
-			sum += checkCoordinate(lines, rowIdx, colIdx)
+			c := Coords{rowIdx, colIdx}
+			for _, dir := range directions {
+				sum += findWord(lines, xmas, c, dir)
+			}
 		}
 	}
-
 	return fmt.Sprintf("%d", sum)
 }
 
 func PartTwo(lines []string) string {
-	return "TODO"
+	sum := 0
+	for rowIdx, row := range lines {
+		for colIdx := range len(row) {
+			c := Coords{rowIdx, colIdx}
+			if string(lines[c.row][c.col]) != "A" {
+				continue
+			}
+			sub := 0
+			for _, dir := range ordinals {
+				newCoords := Coords{c.row + dir.y, c.col + dir.x}
+				delta := Direction{-dir.y, -dir.x}
+				sub += findWord(lines, MAS, newCoords, delta)
+				if sub >= 2 {
+					sum += 1
+					break
+				}
+			}
+		}
+	}
+	return fmt.Sprintf("%d", sum)
 }
