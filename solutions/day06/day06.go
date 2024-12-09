6ac04cd555da0ff77d9423a64a85fa150340c9fc
al-ce <aceserani@gmail.com>
2024-12-09 17:25:56 -0600

    feat(6-2): use goroutines to bruteforce (wrong answer!)

--- solutions/day06/day06.go
+++ solutions/day06/day06.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"os/exec"
 	"slices"
+	"sync"
 	"time"
 )
 
@@ -48,7 +49,7 @@ func printLab(lab [][]rune) {
 	for _, row := range lab {
 		fmt.Println(string(row))
 	}
-	time.Sleep(50 * time.Millisecond)
+	time.Sleep(20 * time.Millisecond)
 }
 
 type Coord struct {
@@ -57,10 +58,11 @@ type Coord struct {
 }
 
 type Guard struct {
-	loc   Coord
-	delta Coord
-	char  rune
-	lab   [][]rune
+	loc     Coord
+	delta   Coord
+	char    rune
+	lab     [][]rune
+	visited map[Coord]Coord
 }
 
 var directions = []Coord{
@@ -77,9 +79,10 @@ func (g *Guard) turnRight() {
 // Mark the current location as visited
 func (g *Guard) visit() {
 	g.lab[g.loc.y][g.loc.x] = 'x'
+	g.visited[g.loc] = g.delta
 }
 
-// Mark the current location as visited and move to the next
+// Move to the next location
 func (g *Guard) move() {
 	g.loc.x += g.delta.x
 	g.loc.y += g.delta.y
@@ -103,7 +106,8 @@ func (g *Guard) atLabEdge() bool {
 func NewGuard(lines []string) Guard {
 	start := findStart(lines)
 	lab := makeLab(lines)
-	g := Guard{start, directions[0], '⮙', lab}
+	visited := map[Coord]Coord{}
+	g := Guard{start, directions[0], '⮙', lab, visited}
 	g.lab[g.loc.y][g.loc.x] = g.char //  mark the initial location
 	return g
 }
@@ -111,27 +115,32 @@ func NewGuard(lines []string) Guard {
 // Get the sum of visited locations
 func (g *Guard) sumOfVisits() int {
 	visitedCount := 0
-	for i, row := range g.lab {
-		for j := range row {
-			if g.lab[i][j] == 'x' {
-				visitedCount++
-			}
-		}
+	for range g.visited {
+		visitedCount++
 	}
 	return visitedCount
 }
 
-func PartOne(lines []string) string {
-	g := NewGuard(lines)
+// Move the guard along its path until it exits the lab or gets stuck in a loop
+func (g *Guard) patrol() int {
 	for !g.atLabEdge() {
-		// printLab(guard.lab)
+		if dir, exists := g.visited[g.loc]; exists && dir == g.delta {
+			return 1
+		}
+		// printLab(g.lab)
 		g.visit()
 		if g.lookAhead() == '#' {
 			g.turnRight()
 		}
 		g.move()
 	}
-	g.visit() // mark the visit at the lab edge
+	g.visit() // mark edge visit
+	return 0
+}
+
+func PartOne(lines []string) string {
+	g := NewGuard(lines)
+	g.patrol()
 
 	// printLab(guard.lab)
 	// println()
@@ -141,5 +150,43 @@ func PartOne(lines []string) string {
 }
 
 func PartTwo(lines []string) string {
-	return "TODO"
+	// Patrol once to get visited locations
+	g_prime := NewGuard(lines)
+	g_prime.patrol()
+
+	results := make(chan int, len(g_prime.visited))
+
+	var wg sync.WaitGroup
+
+	start := findStart(lines)
+
+	// Launch goroutine for each obstacle placement along guard's path
+	for v, location := range g_prime.visited {
+		if location == start { // don't place obstacle ON the guard
+			continue
+		}
+		i, j := v.y, v.x
+		wg.Add(1)
+		go func(i, j int) {
+			defer wg.Done()
+
+			g := NewGuard(lines)
+			g.lab[i][j] = '#'
+			count := g.patrol()
+
+			results <- count
+		}(i, j)
+	}
+
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+
+	totalCount := 0
+	for count := range results {
+		totalCount += count
+	}
+
+	return fmt.Sprint(totalCount)
 }
