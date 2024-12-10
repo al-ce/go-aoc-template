08225d09efe8cedbaaa0143ae9c3be74f8adc7e1
al-ce <aceserani@gmail.com>
2024-12-09 18:20:38 -0600

    test(7): set part 1

--- solutions/day07/test07/test07.go
+++ solutions/day07/test07/test07.go
@@ -7,10 +7,18 @@ import (
 	day07 "go-aoc-template/solutions/day07"
 )
 
-var lines = strings.Split(`example input`, "\n")
+var lines = strings.Split(`190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20`, "\n")
 
 var (
-	partOneAnswer = "example answer"
+	partOneAnswer = "3749"
 	partTwoAnswer = "example answer"
 )
 
