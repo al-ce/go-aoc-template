1952d17d2b3c45557c66a7d1a30032860189a59d
al-ce <aceserani@gmail.com>
2024-12-10 16:16:22 -0600

    feat(7): part 2 solution

--- solutions/day07/day07.go
+++ solutions/day07/day07.go
@@ -8,18 +8,40 @@ import (
 	h "go-aoc-template/internal/helpers"
 )
 
-func testEquation(value int, ops []int) int {
+func evalEquation(value int, ops []int) int {
 	if len(ops) == 1 {
 		return ops[0]
 	}
 	last := ops[len(ops)-1]
-	adds := testEquation(value-last, ops[:len(ops)-1]) + last
+	adds := evalEquation(value-last, ops[:len(ops)-1]) + last
 	if adds == value {
 		return adds
 	} else {
-		muls := testEquation(value/last, ops[:len(ops)-1]) * last
+		muls := evalEquation(value/last, ops[:len(ops)-1]) * last
 		return muls
 	}
+}
+
+func concat(v1, v2 int) int {
+	v, _ := strconv.Atoi(fmt.Sprintf("%d%d", v1, v2))
+	return v
+}
+
+func evalConcatEquation(value int, ops []int) int {
+	if len(ops) == 1 {
+		return ops[0]
+	}
+	concAddOps := append([]int{ops[0]+ops[1]}, ops[2:]...)
+	if evalConcatEquation(value, concAddOps) == value {
+		return value
+	}
+	concMulOps := append([]int{ops[0]*ops[1]}, ops[2:]...)
+	if evalConcatEquation(value, concMulOps) == value {
+		return value
+	}
+
+	concConcOps := append([]int{concat(ops[0], ops[1])}, ops[2:]...)
+	return evalConcatEquation(value, concConcOps)
 }
 
 func PartOne(lines []string) string {
@@ -28,8 +50,8 @@ func PartOne(lines []string) string {
 		split := strings.Split(equation, ": ")
 		value, _ := strconv.Atoi(split[0])
 		operands := h.ParseIntString(split[1], " ")
-		if result := testEquation(value, operands); result == value {
-			sum = sum + int(value)
+		if result := evalEquation(value, operands); result == value {
+			sum += value
 		} else {
 		}
 	}
@@ -37,5 +59,16 @@ func PartOne(lines []string) string {
 }
 
 func PartTwo(lines []string) string {
-	return "TODO"
+	sum := 0
+	for _, equation := range lines {
+		split := strings.Split(equation, ": ")
+		value, _ := strconv.Atoi(split[0])
+		operands := h.ParseIntString(split[1], " ")
+		if result := evalEquation(value, operands); result == value {
+			sum += value
+		} else if result := evalConcatEquation(value, operands); result == value {
+			sum += value
+		}
+	}
+	return fmt.Sprint(sum)
 }
