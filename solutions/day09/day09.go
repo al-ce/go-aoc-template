cc3c6e404c60b5d2812836307b6dc87e24cf989c
al-ce <aceserani@gmail.com>
2024-12-14 09:19:13 -0600

    feat(9): part 2 solution

--- solutions/day09/day09.go
+++ solutions/day09/day09.go
@@ -47,6 +47,90 @@ func PartOne(lines []string) string {
 	return fmt.Sprintf("%d", checkSum)
 }
 
+// Create representation of file blocks and free space e.g. "0..111....22222"
+// (where '.' is a -1)
+func createBlockMap(diskMap string) []int {
+	blockMap := []int{}
+	var block int
+	id := 0
+
+	for idx, digit := range diskMap {
+		size := int(digit - '0')
+		if idx%2 == 0 {
+			id = idx / 2
+			block = id
+		} else {
+			block = -1
+		}
+
+		for i := 0; i < size; i++ {
+			blockMap = append(blockMap, block)
+		}
+	}
+	return blockMap
+}
+
+func getFileSize(blockMap []int, idx int) int {
+	size := 0
+	id := blockMap[idx]
+	for idx >= 0 && blockMap[idx] == id {
+		size++
+		idx--
+	}
+	return size
+}
+
+func getFreeSpace(blockMap []int, idx int) int {
+	size := 0
+	for blockMap[idx] == -1 {
+		size++
+		idx++
+	}
+	return size
+}
+
+func findNextFile(blockMap []int, idx int) int {
+	for idx >= 0 && blockMap[idx] == -1 {
+		idx--
+	}
+	return idx
+}
+
+// Move file blocks into leftmost free space without fragmentation
+func moveFileBlocks(blockMap []int) {
+	fileStart := findNextFile(blockMap, len(blockMap)-1)
+
+	for fileStart > 0 { // Check files from right to left
+		// Look for sufficient free space from start of disk up to file start
+		freeStart := 0
+		freeSpace := 0
+		fileSize := getFileSize(blockMap, fileStart)
+		for freeStart < fileStart && freeSpace < fileSize {
+			freeStart++
+			freeSpace = getFreeSpace(blockMap, freeStart)
+		}
+
+		if freeStart != fileStart { // Found sufficient free space
+			for i := range fileSize {
+				blockMap[freeStart+i], blockMap[fileStart-i] = blockMap[fileStart-i], blockMap[freeStart+i]
+			}
+		}
+
+		// Move file cursor to the next file
+		fileStart = findNextFile(blockMap, fileStart-fileSize)
+	}
+}
+
 func PartTwo(lines []string) string {
-	return "TODO"
+	blockMap := createBlockMap(lines[0])
+	moveFileBlocks(blockMap)
+
+	sum := 0
+	for i, id := range blockMap {
+		if id != -1 {
+			sum += id * i
+		}
+	}
+
+	return fmt.Sprintf("%d", sum)
 }
--- solutions/day09/day09_alt.go
+++ solutions/day09/day09_alt.go
@@ -1,63 +1,63 @@
-// Alternate solution for day9
-// up to 10x slower, but easier to visualize
-
 package day09
 
-import (
-	"fmt"
-)
+// Alternate solution for day9
 
-// Create representation of file blocks and free space e.g. "0..111....22222"
-// (where '.' is a -1)
-func createBlockMap(diskMap string) []int {
-	blockMap := []int{}
-	var block int
-	id := 0
-
-	for idx, digit := range diskMap {
-		size := int(digit - '0')
-		if idx%2 == 0 {
-			id = idx / 2
-			block = id
-		} else {
-			block = -1
-		}
-
-		for i := 0; i < size; i++ {
-			blockMap = append(blockMap, block)
-		}
-	}
-	return blockMap
-}
-
-// Move file blocks into leftmost free space
-func fragmentFileBlocks(blockMap []int) {
-	i, j := 0, len(blockMap)-1
-	for i < j {
-		if blockMap[i] == -1 {
-			blockMap[i], blockMap[j] = blockMap[j], blockMap[i]
-			for blockMap[j] == -1 {
-				j--
-			}
-		}
-		i++
-	}
-}
-
-func PartOneAlt(lines []string) string {
-	blockMap := createBlockMap(lines[0])
-	fragmentFileBlocks(blockMap)
-
-	sum := 0
-	i := 0
-	for blockMap[i] != -1 {
-		sum += blockMap[i] * i
-		i++
-	}
-
-	return fmt.Sprintf("%d", sum)
-}
-func PartTwoAlt(lines []string) string {
-	return "TODO"
-}
-
+//
+// import (
+// 	"fmt"
+// )
+//
+// // Create representation of file blocks and free space e.g. "0..111....22222"
+// // (where '.' is a -1)
+// func createBlockMap(diskMap string) []int {
+// 	blockMap := []int{}
+// 	var block int
+// 	id := 0
+//
+// 	for idx, digit := range diskMap {
+// 		size := int(digit - '0')
+// 		if idx%2 == 0 {
+// 			id = idx / 2
+// 			block = id
+// 		} else {
+// 			block = -1
+// 		}
+//
+// 		for i := 0; i < size; i++ {
+// 			blockMap = append(blockMap, block)
+// 		}
+// 	}
+// 	return blockMap
+// }
+//
+// // Move file blocks into leftmost free space
+// func fragmentFileBlocks(blockMap []int) {
+// 	i, j := 0, len(blockMap)-1
+// 	for i < j {
+// 		if blockMap[i] == -1 {
+// 			blockMap[i], blockMap[j] = blockMap[j], blockMap[i]
+// 			for blockMap[j] == -1 {
+// 				j--
+// 			}
+// 		}
+// 		i++
+// 	}
+// }
+//
+// func PartOne(lines []string) string {
+// 	blockMap := createBlockMap(lines[0])
+// 	fragmentFileBlocks(blockMap)
+//
+// 	sum := 0
+// 	i := 0
+// 	for blockMap[i] != -1 {
+// 		sum += blockMap[i] * i
+// 		i++
+// 	}
+//
+// 	return fmt.Sprintf("%d", sum)
+// }
+// func PartTwo(lines []string) string {
+// 	return "TODO"
+// }
+//
