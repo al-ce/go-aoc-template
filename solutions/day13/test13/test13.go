7621aa86f2f094ffa0581bbfe016dcdc3adfb4f1
al-ce <aceserani@gmail.com>
2024-12-23 09:48:02 -0600

    test(13): add part 2 lines, refactor runTest to accept different lines

--- solutions/day13/test13/test13.go
+++ solutions/day13/test13/test13.go
@@ -23,14 +23,30 @@ Button A: X+69, Y+23
 Button B: X+27, Y+71
 Prize: X=18641, Y=10279`, "\n")
 
+var lines2 = strings.Split(`Button A: X+94, Y+34
+Button B: X+22, Y+67
+Prize: X=10000000008400, Y=10000000005400
+
+Button A: X+26, Y+66
+Button B: X+67, Y+21
+Prize: X=10000000012748, Y=10000000012176
+
+Button A: X+17, Y+86
+Button B: X+84, Y+37
+Prize: X=10000000007870, Y=10000000006450
+
+Button A: X+69, Y+23
+Button B: X+27, Y+71
+Prize: X=10000000018641, Y=10000000010279`, "\n")
+
 var (
 	partOneAnswer = "480"
-	partTwoAnswer = "example answer"
+	partTwoAnswer = "unknown"
 )
 
 type SolutionFunc func([]string) string
 
-func runTest(part int, solution SolutionFunc, expected string) {
+func runTest(lines []string, part int, solution SolutionFunc, expected string) {
 	fmt.Printf("Part %d: ", part)
 	result := solution(lines)
 	if result != expected {
@@ -41,6 +57,6 @@ func runTest(part int, solution SolutionFunc, expected string) {
 }
 
 func main() {
-	runTest(1, day13.PartOne, partOneAnswer)
-	runTest(2, day13.PartTwo, partTwoAnswer)
+	runTest(lines, 1, day13.PartOne, partOneAnswer)
+	runTest(lines2, 2, day13.PartTwo, partTwoAnswer)
 }
